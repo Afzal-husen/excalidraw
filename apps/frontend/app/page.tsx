c2ffbfd40b8fb20b6c59c0f40ecb3078ccabc5c5
Afzal Diwan <afzalhusen3@gmail.com>
2025-03-24 22:48:09 +0530

    added tailwind v4 and shadcn

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/components/shadcn/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Hello world</Button>
+    </div>
+  );
+};
+
+export default Home;
