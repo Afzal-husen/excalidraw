3f2f632bbf0ed0b934648e6adf22e1d06cc48c27
Afzal Diwan <afzalhusen3@gmail.com>
2025-04-02 21:30:19 +0530

    progress

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,7 @@
 import { WebSocketServer, WebSocket } from "ws";
-import { prisma } from "@repo/db";
+import { prisma, Shape } from "@repo/db";
 import jwt from "jsonwebtoken";
+
 const wss = new WebSocketServer({ port: 8080 });
 
 interface CustomJwtPayload extends jwt.JwtPayload {
@@ -39,21 +40,48 @@ const checkUser = async (token: string): Promise<string | null> => {
   }
 };
 
+const addShape = async (
+  shapeString: string,
+  room_name: string,
+): Promise<Shape | null> => {
+  try {
+    const shape = await prisma.shape.create({
+      data: {
+        shape_type: shapeString,
+        room: {
+          connect: {
+            name: room_name,
+          },
+        },
+      },
+    });
+    return shape;
+  } catch (error) {
+    return null;
+  }
+};
+
 wss.on("connection", async (socket, request) => {
   const authorization = request.headers.authorization;
   if (!authorization || !authorization.startsWith("Bearer ")) {
+    console.log("User not found");
+    socket.close();
     return;
   }
 
   const token = authorization.split(" ")[1];
 
   if (!token) {
+    console.log("User not found");
+    socket.close();
     return;
   }
 
   const userId = await checkUser(token);
 
   if (!userId) {
+    console.log("User not found");
+    socket.close();
     return;
   }
 
@@ -63,22 +91,37 @@ wss.on("connection", async (socket, request) => {
     rooms: [],
   });
 
-  socket.on("message", (data) => {
+  socket.on("message", async (data) => {
     const parsedData = JSON.parse(data.toString());
     const { type, content, room_name } = parsedData;
     const user = users.find((x) => x.socket === socket);
 
     if (type === "join_room") {
       if (!user) {
+        socket.close();
         return;
       }
       user.rooms.push(room_name);
+      console.log("User joined room", room_name);
     } else if (type === "leave_room") {
       if (!user) {
         return;
       }
       user.rooms = user.rooms.filter((x) => x !== room_name);
     } else if (type === "draw") {
+      const shapeString = JSON.stringify(content);
+      const shape = {
+        type,
+        content: shapeString,
+        room_name,
+      };
+      users.forEach((x) => {
+        if (x.rooms.includes(room_name)) {
+          x.socket.send(JSON.stringify(shape));
+        }
+      });
+
+      await addShape(shapeString, room_name);
     }
   });
 });
